3a286d6c55e0eadabbe8daf1c40cbebc3d59fea7
Mercy Nwoji <98951821+merbear01@users.noreply.github.com>
2024-04-26 20:30:55 +0100

    Add files via upload

---
+++ ATLA.py
@@ -0,0 +1,25 @@
+def main():
+    print(Aang("Mercy"))
+    print(Azula())
+
+def Aang(name):
+    return f'{name}, will you help me defeat fire lord Ozai??'
+
+
+def katara(name):
+    return f'The fire nation took my mother away from me,{name}'
+
+def zuko():
+    return f'i must restore my honour'
+
+def toph():
+    return f'i am not toph i am melon lord mwahahah'
+
+
+def sokka():
+    return f'Just a cool dude with my cool boomerang'
+
+def Azula():
+    return "Trust Is for Fools, Fear Is the Only Reliable Way"
+if __name__=="__main__":
+    main()
